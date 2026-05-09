dac99f5a725ae2e0a77657448ae81cf0c31b9fd1
Usu171 <usu171@proton.me>
2026-05-09 14:24:18 +0800

    fix: ssh permission error

--- home/shared/ssh.nix
+++ home/shared/ssh.nix
@@ -1,4 +1,4 @@
-_:
+{ lib, ... }:
 
 {
   programs.ssh = {
@@ -27,6 +27,30 @@ _:
         hostname = "192.168.1.5";
         user = "usu171";
       };
+
+      "github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+      };
     };
+  };
+
+
+  # ssh config 权限问题 https://github.com/nix-community/home-manager/issues/322#issuecomment-3730266609
+  home.file = {
+    # home-manager wrongly thinks it doesn't manage (and thus shouldn't clobber) this file due to the activation script
+    ".ssh/config".force = true;
+  };
+
+  home.activation = {
+    # https://github.com/nix-community/home-manager/issues/322
+    fixSshPermissions = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      run install -d -m 0700 "$HOME/.ssh"
+      if [ -L "$HOME/.ssh/config" ]; then
+        src="$(readlink -f "$HOME/.ssh/config")"
+        run rm -f "$HOME/.ssh/config"
+        run install -m 0600 "$src" "$HOME/.ssh/config"
+      fi
+    '';
   };
 }
