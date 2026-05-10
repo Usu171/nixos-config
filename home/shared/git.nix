6837e383aaafb63418f6bf53716bd89d7ce6ac3c
Usu171 <usu171@proton.me>
2026-05-10 12:54:37 +0800

    feat(home): git ssh signing

--- home/shared/git.nix
+++ home/shared/git.nix
@@ -7,5 +7,10 @@ _:
       name = "Usu171";
       email = "usu171@proton.me";
     };
+    settings.gpg.format = "ssh";
+    signing = {
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
   };
 }
