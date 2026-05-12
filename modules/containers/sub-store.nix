7f4290616a9cf3f85f62b3cd60f56b22ed9bc4ff
Usu171 <usu171@proton.me>
2026-05-12 23:02:43 +0800

    feat: containers/sub-store.nix

---
+++ modules/containers/sub-store.nix
@@ -0,0 +1,39 @@
+_:
+
+{
+  systemd.tmpfiles.rules = [
+    "d /var/lib/sub-store-data 0750 root root - -"
+  ];
+
+  virtualisation.oci-containers.containers.sub-store = {
+    image = "docker.io/xream/sub-store:latest";
+    autoStart = true;
+
+    ports = [
+      "0.0.0.0:3001:3001"
+    ];
+
+    volumes = [
+      "/var/lib/sub-store-data:/opt/app/data:Z"
+    ];
+
+    environment = {
+      SUB_STORE_FRONTEND_HOST = "0.0.0.0";
+      SUB_STORE_FRONTEND_PORT = "3001";
+      SUB_STORE_FRONTEND_BACKEND_PATH = "/vQ8kR2mN7pL4xT9c";
+      SUB_STORE_BACKEND_SYNC_CRON = "55 23 * * *";
+      SUB_STORE_BACKEND_DEFAULT_PROXY = "http://host.containers.internal:7897";
+    };
+
+    extraOptions = [
+      "--replace"
+      "--pull=always"
+      "--log-opt=max-size=10m"
+      "--log-opt=max-file=3"
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    3001
+  ];
+}
