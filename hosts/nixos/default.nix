628d514731c69ba455ab30cd2e414923546bde08
Usu171 <usu171@proton.me>
2026-05-13 16:53:07 +0800

    feat(nixos): consoleblank

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -11,6 +11,8 @@
 
   networking.hostName = "nixos";
 
+  boot.kernelParams = [ "consoleblank=300" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
