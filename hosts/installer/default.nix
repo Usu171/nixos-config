3d4a03bfd2e16ed227d2dc4450a0f050252165e4
Usu171 <usu171@proton.me>
2026-05-15 23:37:39 +0800

    feat:(installer): nix.settings

--- hosts/installer/default.nix
+++ hosts/installer/default.nix
@@ -30,11 +30,16 @@ in
   networking.hostName = "installer";
 
   nix.settings = {
+    substituters = [
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     experimental-features = [
       "nix-command"
       "flakes"
     ];
-    trusted-users = [ "root" ];
   };
 
   # If hosts/iso/mihomo exists, bake its contents into the ISO and seed the
