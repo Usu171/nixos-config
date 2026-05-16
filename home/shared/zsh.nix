a3c6918c7d9fcc5a516ee5cec6f091b8de71063d
Usu171 <usu171@proton.me>
2026-05-16 22:07:28 +0800

    feat: using home manager manage zsh, rm zinit

--- home/shared/zsh.nix
+++ home/shared/zsh.nix
@@ -1,19 +1,100 @@
-{ homeDirectory, ... }:
+{
+  homeDirectory,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  ohMyZsh = "${pkgs.oh-my-zsh}/share/oh-my-zsh";
+in
 
 {
   home.file.".p10k.zsh".source = ../../dotfiles/.p10k.zsh;
-  # home.file.".zshrc".source = ../dotfiles/.zshrc;
+  home.packages = [
+    pkgs.git-open
+  ];
 
   programs.zsh = {
     enable = true;
-    initContent = ''
-      source ${../../dotfiles/.zshrc}
-      export PNPM_HOME="${homeDirectory}/.local/share/pnpm"
-      case ":$PATH:" in
-        *":$PNPM_HOME:"*) ;;
-        *) export PATH="$PNPM_HOME:$PATH" ;;
-      esac
-    '';
+    autosuggestion.enable = true;
+    plugins = [
+      {
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
+      {
+        name = "history-substring-search";
+        src = pkgs.zsh-history-substring-search;
+        file = "share/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh";
+      }
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "fast-syntax-highlighting";
+        src = pkgs.zsh-fast-syntax-highlighting;
+        file = "share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh";
+      }
+    ];
+    initContent = lib.mkMerge [
+      (lib.mkOrder 10 ''
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+        fi
+      '')
+
+      (lib.mkOrder 550 ''
+        fpath+=(${pkgs.zsh-completions}/share/zsh/site-functions)
+
+        # $ENABLE_CORRECTION == "true" # correction 开关
+
+        source ${ohMyZsh}/lib/async_prompt.zsh # 异步执行
+        # source ${ohMyZsh}/lib/bzr.zsh # Bazaar 已经似了
+        # source ${ohMyZsh}/lib/cli.zsh # omz 命令行工具 管理 omz 的
+        source ${ohMyZsh}/lib/clipboard.zsh # clipcopy clippaste 系统剪贴板复制粘贴
+        # source ${ohMyZsh}/lib/compfix.zsh # 检测和处理不安全的补全目录
+        # source ${ohMyZsh}/lib/completion.zsh # 补全
+        # source ${ohMyZsh}/lib/correction.zsh # 纠错 cp man mkdir mv sudo su
+
+        # source ${ohMyZsh}/lib/diagnostics.zsh # omz诊断工具 omz_diagnostic_dump
+        source ${ohMyZsh}/lib/directories.zsh # 目录 ... n='cd -n'
+        source ${ohMyZsh}/lib/functions.zsh # 一些实用函数 open take* mkcd
+        # source ${ohMyZsh}/lib/git.zsh # git信息获取显示
+        # source ${ohMyZsh}/lib/grep.zsh # rg：？
+        source ${ohMyZsh}/lib/history.zsh # 历史记录配置 时间戳 等
+        source ${ohMyZsh}/lib/key-bindings.zsh # 快捷键设置 emacs key bindings bindkey -e
+        source ${ohMyZsh}/lib/misc.zsh # 杂项 小工具 依赖functions
+        # source ${ohMyZsh}/lib/nvm.zsh # nvm
+        # source ${ohMyZsh}/lib/prompt_info_functions.zsh # 获取编程语言版本信息
+        # source ${ohMyZsh}/lib/spectrum.zsh # 颜色支持库
+        source ${ohMyZsh}/lib/termsupport.zsh # 终端窗口标题设置 更新
+        # source ${ohMyZsh}/lib/theme-and-appearance.zsh # omz 主题外观库
+        # source ${ohMyZsh}/lib/vcs_info.zsh # 补丁修复 CVE-2021-45444 漏洞，确保 vcs_info 输出中的 % 字符被正确转义，防止潜在的命令注入攻击 zsh versions from 5.0.3 to 5.8.
+
+        source ${ohMyZsh}/plugins/git/git.plugin.zsh # git简写命令 https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
+        # source ${ohMyZsh}/plugins/pip/pip.plugin.zsh # pip补全,简写命令 https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/pip
+        # source ${ohMyZsh}/plugins/command-not-found/command-not-found.plugin.zsh # 插件会在输入不存在的命令时提示安装哪个包，使用programs.command-not-found https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/command-not-found/README.md
+        # 应当使用 https://github.com/nix-community/nix-index-database 自带 command-not-found shell集成
+        # 而不是programs.command-not-found
+
+      '')
+
+      (lib.mkAfter ''
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+        source ${../../dotfiles/.zshrc}
+
+        export PNPM_HOME="${homeDirectory}/.local/share/pnpm"
+        case ":$PATH:" in
+          *":$PNPM_HOME:"*) ;;
+          *) export PATH="$PNPM_HOME:$PATH" ;;
+        esac
+      '')
+    ];
     profileExtra = ''
       if [[ "$XDG_CURRENT_DESKTOP" != "KDE" ]]; then
         export QT_QPA_PLATFORMTHEME=qt6ct
---
+++ old/zshrc_plugin.zsh
@@ -0,0 +1,82 @@
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+### End of Zinit's installer chunk
+
+
+
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+
+
+# $ENABLE_CORRECTION == "true" # correction 开关
+
+zinit snippet OMZL::async_prompt.zsh # 异步执行
+# zinit snippet OMZL::bzr.zsh # Bazaar 已经似了
+# zinit snippet OMZL::cli.zsh # omz 命令行工具 管理 omz 的
+zinit snippet OMZL::clipboard.zsh # clipcopy clippaste 系统剪贴板复制粘贴
+# zinit snippet OMZL::compfix.zsh # 检测和处理不安全的补全目录
+# zinit snippet OMZL::completion.zsh # 补全
+# zinit snippet OMZL::correction.zsh # 纠错 cp man mkdir mv sudo su
+
+# zinit snippet OMZL::diagnostics.zsh # omz诊断工具 omz_diagnostic_dump
+zinit snippet OMZL::directories.zsh # 目录 ... n='cd -n'
+zinit snippet OMZL::functions.zsh # 一些实用函数 open take* mkcd
+# zinit snippet OMZL::git.zsh # git信息获取显示
+# zinit snippet OMZL::grep.zsh # rg：？
+zinit snippet OMZL::history.zsh # 历史记录配置 时间戳 等
+zinit snippet OMZL::key-bindings.zsh # 快捷键设置 emacs key bindings bindkey -e
+zinit snippet OMZL::misc.zsh # 杂项 小工具 依赖functions
+# zinit snippet OMZL::nvm.zsh # nvm
+# zinit snippet OMZL::prompt_info_functions.zsh # 获取编程语言版本信息
+# zinit snippet OMZL::spectrum.zsh # 颜色支持库
+zinit snippet OMZL::termsupport.zsh # 终端窗口标题设置 更新
+# zinit snippet OMZL::theme-and-appearance.zsh # omz 主题外观库
+# zinit snippet OMZL::vcs_info.zsh # 补丁修复 CVE-2021-45444 漏洞，确保 vcs_info 输出中的 % 字符被正确转义，防止潜在的命令注入攻击 zsh versions from 5.0.3 to 5.8.
+
+zinit snippet OMZP::git # git简写命令 https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
+# zinit snippet OMZP::pip # pip补全,简写命令 https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/pip
+# zinit snippet OMZP::command-not-found # 插件会在输入不存在的命令时提示安装哪个包，使用programs.command-not-found https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/command-not-found/README.md
+# 应当使用 https://github.com/nix-community/nix-index-database 自带 command-not-found shell集成
+# 而不是programs.command-not-found
+
+
+
+zinit light paulirish/git-open # git open 打开GitHub页面
+zinit light zsh-users/zsh-autosuggestions # 灰色提示历史命令
+zinit light zsh-users/zsh-completions # 额外命令补全
+
+zinit light zdharma-continuum/fast-syntax-highlighting # 语法高亮
+zinit light zsh-users/zsh-history-substring-search # 历史子字符串搜索
+
+zinit ice depth=1
+zinit light romkatv/powerlevel10k # 主题
+
+zinit light Aloxaf/fzf-tab # fzf-tab 补全
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
