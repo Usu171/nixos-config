059506525b7d287c78f7eca99aa16cf929bb098e
Usu171 <usu171@proton.me>
2026-05-17 14:02:46 +0800

    feat: git maintenance manyFiles

--- home/shared/git.nix
+++ home/shared/git.nix
@@ -3,14 +3,18 @@ _:
 {
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "Usu171";
-      email = "usu171@proton.me";
+    settings = {
+      user = {
+        name = "Usu171";
+        email = "usu171@proton.me";
+      };
+      gpg.format = "ssh";
+      feature.manyFiles=true;
     };
-    settings.gpg.format = "ssh";
     signing = {
       key = "~/.ssh/id_ed25519.pub";
       signByDefault = true;
     };
+    maintenance.enable = true;
   };
 }
