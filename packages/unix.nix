31c7103ba535d2f21446a369b1c23c3ab6c6e385
Usu171 <usu171@proton.me>
2026-05-29 16:39:15 +0800

    feat(unix): mpv obs

--- packages/unix.nix
+++ packages/unix.nix
@@ -7,7 +7,16 @@
     deskflow
     xwayland-satellite
     wl-clipboard
+    mpv
   ];
+
+  programs.obs-studio = {
+    enable = true;
+    enableVirtualCamera = true;
+    package = pkgs.obs-studio.override {
+      cudaSupport = true;
+    };
+  };
 
   networking.firewall = {
     allowedTCPPorts = [ 24800 ];
