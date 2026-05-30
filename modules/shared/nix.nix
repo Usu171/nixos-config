937d21cc3ea64928e2cb98e08d9de53bf30baa21
Usu171 <usu171@proton.me>
2026-05-30 12:10:32 +0800

    chore: substituters

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -22,8 +22,14 @@
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://nix-community.cachix.org"
+      "https://cache.numtide.com"
+      # "https://install.determinate.systems"
     ];
-    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      # "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+      ];
     experimental-features = [
       "nix-command"
       "flakes"
