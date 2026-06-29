e508db237ba3532d18fae7fab0d79edac13ac52a
Usu171 <usu171@proton.me>
2026-06-29 17:12:32 +0800

    feat(nixos): bluetooth

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -11,6 +11,8 @@
 
   boot.kernelParams = [ "consoleblank=300" ];
 
+  hardware.bluetooth.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
